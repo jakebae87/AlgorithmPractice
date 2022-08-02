a566625003c100367814b31162db7cc4f4ad18d7
(hooray) <(baeddi87@naver.com)>
2022-08-03 01:15:08 +0900

    행렬의 덧셈

---
+++ Programmers/src/level1/ArraySum.java
@@ -0,0 +1,24 @@
+package level1;
+
+public class ArraySum {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+
+		int[][] answer = new int[arr1.length][arr2[0].length];
+
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[0].length; j++)
+
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		ArraySum as = new ArraySum();
+
+		int[][] arr1 = { { 1, 2 }, { 2, 3 } };
+		int[][] arr2 = { { 3, 4 }, { 5, 6 } };
+		as.solution(arr1, arr2);
+	}
+}
