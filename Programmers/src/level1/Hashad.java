9dc9eb3011b375aecdcfe07ba8fad7b8c67cdb23
(hooray) <(baeddi87@naver.com)>
2022-08-17 17:43:17 +0900

    Hashad version 1

--- Programmers/src/level1/Hashad.java
+++ Programmers/src/level1/Hashad.java
@@ -1,9 +1,33 @@
 package level1;
 
+import java.util.ArrayList;
+
 public class Hashad {
 	public boolean solution(int x) {
 		boolean answer = true;
-		
+
+		String x_ = String.valueOf(x);
+		int sum = 0;
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (int i = 0; i < x_.length(); i++) {
+			list.add(String.valueOf(x_.charAt(i)));
+			for (int j = 0; j < list.size(); j++) {
+				sum += Integer.parseInt(list.get(j));
+			}
+		}
+
+		if (x % sum == 0) {
+			answer = true;
+		} else {
+			answer = false;
+		}
+
 		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Hashad hashad = new Hashad();
+		System.out.println(hashad.solution(10));
 	}
 }
